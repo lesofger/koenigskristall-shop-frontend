294e1641386f31875e43c7d820b7f37f7f04a6cf
Yqod <phoneticc@web.de>
2025-08-31 09:44:09 +0200

    centering navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -140,7 +140,7 @@ const Navbar = () => {
   return (
     <>
       <nav className="sticky top-0 z-50 bg-background/95 backdrop-blur-sm border-b border-border">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto px-4 sm:px-2 lg:px-2">
           <div className="flex items-center justify-between h-16">
             {/* Logo */}
             <Link to="/" className="flex items-center space-x-2">
@@ -153,7 +153,7 @@ const Navbar = () => {
             </Link>
 
             {/* Desktop Navigation */}
-            <div className="hidden md:flex items-center space-x-8">
+            <div className="hidden pr-28 md:flex items-center space-x-8">
               {navItems.map((item) => (
                 <Link
                   key={item.name}
